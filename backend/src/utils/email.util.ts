b180c2b382616f7f5e74b15aacf01d8e923ef846
Ogbonna Blessed <blessedlyrics11@gmail.com>
2026-04-10 05:14:25 +0100

    refactor: replace smtp with resend

--- backend/src/utils/email.util.ts
+++ backend/src/utils/email.util.ts
@@ -1,35 +1,19 @@
+import { Resend } from "resend";
 import dotenv from "dotenv";
 dotenv.config();
 
-import nodemailer from "nodemailer";
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
-export const transporter = nodemailer.createTransport({
-  host: "smtp.mail.yahoo.com",
-  port: 465, // or 587
-  secure: true, // true for 465, false for 587
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-
-export const sendEmail = async (
-  to: string,
-  subject: string,
-  html: string,
-) => {
+export const sendEmail = async (to: string, subject: string, html: string) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"Lekota" <${process.env.EMAIL_USER}>`,
+    const response = await resend.emails.send({
+      from: "Lekota <blessedlyrics11@omegavisionchristiannetwork.com>", // can be any verified email in Resend
       to,
       subject,
       html,
     });
-
-    console.log("Email sent:", info.messageId);
+    
+    return response;
   } catch (error: any) {
     console.error("Email failed:", error.message);
     throw error;
